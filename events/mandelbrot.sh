505e9c7e9bc18ae989a604433903b4f793293979
Alexander Mead <alexander@digilab.co.uk>
2023-03-09 17:16:53 +0000

    tidy

--- events/mandelbrot.sh
+++ events/mandelbrot.sh
@@ -4,8 +4,8 @@ curl -i -X POST http://127.0.0.1:3000/mandelbrot \
     -H 'Content-Type: application/json' \
     --data-binary @- << EOF
 { 
-    "real":"-0.5", 
-    "imag":"0.", 
-    "size":"2." 
+    "real": "-0.5", 
+    "imag": "0.", 
+    "size": "2." 
 }
 EOF
